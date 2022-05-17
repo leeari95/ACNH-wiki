e6a9335516fe1863d13110d8066eabca0047a326
Ari <lee_ari95@icloud.com>
2022-05-17 15:22:59 +0900

    ✨ [feat] 물고기 목록을 불러오는 Request, Response를 생성 #1

---
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/Networking/Request/FishRequest.swift
@@ -0,0 +1,19 @@
+//
+//  FishRequest.swift
+//  Animal-Crossing-Wiki
+//
+//  Created by Ari on 2022/05/17.
+//
+
+import Foundation
+
+struct FishRequest: APIRequest {
+    typealias Response = FishResponseDTO
+    let method: HTTPMethod = .get
+    let baseURL: URL? = URL(string: EnvironmentsVariable.repoURL)
+    let path: String = "Fish.json"
+    var headers: [String : String]? = [:]
+    var parameters: [String : String] {
+        return [:]
+    }
+}
---
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/Networking/Response/FishResponseDTO.swift
@@ -0,0 +1,106 @@
+//
+//  FishResponseDTO.swift
+//  Animal-Crossing-Wiki
+//
+//  Created by Ari on 2022/05/17.
+//
+
+import Foundation
+
+// MARK: - FishResponseDTO
+struct FishResponseDTO: Codable, APIResponse {
+    let num: Int
+    let name: String
+    let iconImage, critterpediaImage, furnitureImage: String
+    let sell: Int
+    let whereHow: WhereHow
+    let shadow: Shadow
+    let catchDifficulty: CatchDifficulty
+    let vision: Vision
+    let totalCatchesToUnlock: Int
+    let spawnRates: String
+    let size: Size
+    let surface: Bool
+    let description: [String]
+    let catchPhrase: [String]
+    let hhaBasePoints: Int
+    let lightingType: LightingType?
+    let iconFilename, critterpediaFilename, furnitureFilename: String
+    let internalId: Int
+    let uniqueEntryId: String
+    let translations: Translations
+    let hemispheres: Hemispheres
+    let colors: [Color]
+}
+
+enum CatchDifficulty: String, Codable {
+    case easy = "Easy"
+    case hard = "Hard"
+    case medium = "Medium"
+    case veryEasy = "Very Easy"
+    case veryHard = "Very Hard"
+}
+
+// MARK: - Hemispheres
+struct Hemispheres: Codable {
+    let north: EmergenceInfo
+    let south: EmergenceInfo
+}
+
+// MARK: - North
+struct EmergenceInfo: Codable {
+    let time: [Time]
+    let timeArray: [Int]
+    let months: [String]
+    let monthsArray: [Int]
+}
+
+enum Time: String, Codable {
+    case allDay = "All day"
+    case the4Am9Pm = "4 AM – 9 PM"
+    case the4Pm9Am = "4 PM – 9 AM"
+    case the9Am4Pm = "9 AM – 4 PM"
+    case the9Pm4Am = "9 PM – 4 AM"
+    case time9Pm4Am = "9 PM – 4 AM"
+}
+
+enum LightingType: String, Codable {
+    case emission = "Emission"
+    case fluorescent = "Fluorescent"
+}
+
+enum Shadow: String, Codable {
+    case large = "Large"
+    case long = "Long"
+    case medium = "Medium"
+    case small = "Small"
+    case xLarge = "X-Large"
+    case xLargeWFin = "X-Large w/Fin"
+    case xSmall = "X-Small"
+    case xxLarge = "XX-Large"
+}
+
+enum Size: String, Codable {
+    case the1X1 = "1x1"
+    case the2X1 = "2x1"
+    case the2X2 = "2x2"
+    case the3X2 = "3x2"
+}
+
+enum Vision: String, Codable {
+    case medium = "Medium"
+    case narrow = "Narrow"
+    case veryNarrow = "Very Narrow"
+    case veryWide = "Very Wide"
+    case wide = "Wide"
+}
+
+enum WhereHow: String, Codable {
+    case pier = "Pier"
+    case pond = "Pond"
+    case river = "River"
+    case riverClifftop = "River (clifftop)"
+    case riverMouth = "River (mouth)"
+    case sea = "Sea"
+    case seaRainyDays = "Sea (rainy days)"
+}
