67e9e318ab3e5608a97ddd2250b2433141931def
Ari <lee_ari95@icloud.com>
2022-06-15 15:24:12 +0900

    ✨ [feat] 커스텀으로 제작한 SectionView를 생성 #6
    - SectionView 상단에 위치하는 SectionHeaderView를 구현
    - UI 관련된 타입 extension 하여 기능 추가

---
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/Extension/UIStackView+extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIStackView+extension.swift
+//  Animal-Crossing-Wiki
+//
+//  Created by Ari on 2022/06/14.
+//
+
+import UIKit
+
+extension UIStackView {
+    func addArrangedSubviews(_ views: UIView...) {
+        views.forEach { view in
+            view.translatesAutoresizingMaskIntoConstraints = false
+            addArrangedSubview(view)
+        }
+    }
+}
