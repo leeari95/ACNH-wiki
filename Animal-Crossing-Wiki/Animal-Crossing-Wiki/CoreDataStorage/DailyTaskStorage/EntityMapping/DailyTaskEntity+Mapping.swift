0d0ffa8fe099e6e5f3c27f26a9e0cd2a647b4b2f
Ari <lee_ari95@icloud.com>
2022-06-17 16:06:19 +0900

    ♻️ [refactor] DailyTask 타입 속성을 추가 #3 #4
    - CoreData 모델 타입도 동일하게 속성 동기화 처리

--- Animal-Crossing-Wiki/Animal-Crossing-Wiki/CoreDataStorage/DailyTaskStorage/EntityMapping/DailyTaskEntity+Mapping.swift
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/CoreDataStorage/DailyTaskStorage/EntityMapping/DailyTaskEntity+Mapping.swift
@@ -13,11 +13,18 @@ extension DailyTaskEntity {
     convenience init(_ task: DailyTask, context: NSManagedObjectContext) {
         self.init(context: context)
         self.id = task.id
+        self.name = task.name
         self.icon = task.icon
         self.isCompleted = task.isCompleted
+        self.amount = Int64(task.amount)
     }
     
     func toDomain() -> DailyTask {
-        return DailyTask(icon: self.icon ?? "", isCompleted: self.isCompleted)
+        return DailyTask(
+            name: self.name ?? "",
+            icon: self.icon ?? "",
+            isCompleted: self.isCompleted,
+            amount: Int(self.amount)
+        )
     }
 }
--- Animal-Crossing-Wiki/Animal-Crossing-Wiki/Models/DailyTask.swift
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/Models/DailyTask.swift
@@ -9,8 +9,10 @@ import Foundation
 
 struct DailyTask {
     let id: UUID = UUID()
+    let name: String
     let icon: String
     private(set) var isCompleted: Bool
+    let amount: Int
     
     mutating func toggleCompleted() {
         self.isCompleted.toggle()
