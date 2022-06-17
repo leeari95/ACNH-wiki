c780e7efdd40472e985d5348664f721c7f4c2f5a
Ari <lee_ari95@icloud.com>
2022-06-17 11:20:49 +0900

    🔧 [chore] UIButton extension 코드를 따로 분리 #6

--- Animal-Crossing-Wiki/Animal-Crossing-Wiki/Dashboard/Views/VillagerButton.swift
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/Dashboard/Views/VillagerButton.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import Kingfisher
 
 class VillagerButton: UIButton {
     
@@ -40,19 +39,5 @@ extension VillagerButton {
         self.init(frame: .zero)
         setImage(with: vilager.iconImage)
         self.villager = vilager
-    }
-}
-
-extension UIButton {
-    func setImage(with urlString: String) {
-        ImageCache.default.retrieveImage(forKey: urlString) { result in
-            if let image = try? result.get().image?.withRenderingMode(.alwaysOriginal) {
-                self.setImage(image, for: .normal)
-            } else {
-                let resource = URL(string: urlString)
-                    .flatMap { ImageResource(downloadURL: $0, cacheKey: urlString) }
-                self.kf.setImage(with: resource, for: .normal)
-            }
-        }
     }
 }
