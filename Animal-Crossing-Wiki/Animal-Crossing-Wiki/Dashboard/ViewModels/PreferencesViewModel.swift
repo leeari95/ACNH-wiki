03cff8c6bdd19bfec6577051cfeee84786cf0195
Ari <lee_ari95@icloud.com>
2022-06-23 15:13:55 +0900

    🔧 [chore] 사용자의 게임 정보에 대한 ViewModel 초기 생성 #7

---
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/Dashboard/ViewModels/PreferencesViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  PreferencesSectionViewModel.swift
+//  Animal-Crossing-Wiki
+//
+//  Created by Ari on 2022/06/21.
+//
+
+import Foundation
+import RxSwift
+import RxRelay
+
+final class PreferencesViewModel {
+    
+}
---
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/Dashboard/ViewModels/UserInfoSectionViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  UserInfoSectionViewModel.swift
+//  Animal-Crossing-Wiki
+//
+//  Created by Ari on 2022/06/21.
+//
+
+import Foundation
+
+final class UserInfoSectionViewModel {
+    
+}
