4c64fcd16fba6baaa8238a60a706df11f7a07e87
Ari <lee_ari95@icloud.com>
2022-06-28 17:50:22 +0900

    ✨ [feat] VillagersSection 내부에 UI를 추가적으로 구현 #6
    - 가져온 Vilagers가 비어있을 경우 Label만 표시될 수 있도록 구현
    - 가져온 Villagers가 비어있지 않다면 동물들의 아이콘을 보여주도록 구현
    - 체크표시 기능, Reset 버튼을 추가적으로 구현

--- Animal-Crossing-Wiki/Animal-Crossing-Wiki/Dashboard/Views/VillagersSection.swift
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/Dashboard/Views/VillagersSection.swift
@@ -26,6 +26,27 @@ class VillagersSection: UIView {
         return collectionView
     }()
     
+    private lazy var resetButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Reset", for: .normal)
+        button.setTitleColor(.acText, for: .normal)
+        button.titleLabel?.font = .preferredFont(for: .footnote, weight: .semibold)
+        button.backgroundColor = .acText.withAlphaComponent(0.2)
+        button.layer.cornerRadius = 12
+        button.widthAnchor.constraint(equalToConstant: 56).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 28).isActive = true
+        return button
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = .preferredFont(forTextStyle: .footnote)
+        label.textColor = .acText
+        label.numberOfLines = 0
+        return label
+    }()
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
@@ -37,21 +58,26 @@ class VillagersSection: UIView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        let contentHeight = self.collectionView.collectionViewLayout.collectionViewContentSize.height
-        self.heightConstraint.constant = contentHeight == .zero ? 40 : contentHeight
+        let contentHeight = self.collectionView.collectionViewLayout.collectionViewContentSize.height + 80
+        self.heightConstraint.constant = contentHeight == .zero ? 80 : contentHeight
     }
     
     override func layoutIfNeeded() {
         super.layoutIfNeeded()
-        self.heightConstraint.constant = self.collectionView.collectionViewLayout.collectionViewContentSize.height
+        self.heightConstraint.constant = self.collectionView.collectionViewLayout.collectionViewContentSize.height + 80
     }
     
     private func configure() {
-        addSubviews(collectionView)
+        addSubviews(collectionView, resetButton, descriptionLabel)
 
-        self.heightConstraint = self.collectionView.heightAnchor.constraint(equalToConstant: .zero)
+        self.heightConstraint = self.collectionView.heightAnchor.constraint(equalToConstant: 80)
         heightConstraint.priority = .defaultHigh
         NSLayoutConstraint.activate([
+            resetButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
+            resetButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            descriptionLabel.bottomAnchor.constraint(equalTo: resetButton.topAnchor, constant: -15),
+            descriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
             collectionView.topAnchor.constraint(equalTo: topAnchor),
             collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
             collectionView.widthAnchor.constraint(equalTo: widthAnchor),
@@ -77,8 +103,32 @@ class VillagersSection: UIView {
         output?.villagers
             .observe(on: MainScheduler.instance)
             .withUnretained(self)
-            .subscribe(onNext: { owner, _ in
+            .subscribe(onNext: { owner, villagers in
                 owner.layoutIfNeeded()
+                if villagers.isEmpty {
+                    owner.subviews.compactMap { $0 as? UIButton }.first?.removeFromSuperview()
+                    owner.descriptionLabel.bottomAnchor.constraint(equalTo: owner.bottomAnchor, constant: -15).isActive = true
+                    owner.descriptionLabel.text = """
+                    Who have you talked to today?
+                    Find the villagers you have visited and tap the home icon on the villager's page to keep track.
+                    """
+                } else {
+                    owner.descriptionLabel.text = "Long press on a villager to see more info about them."
+                    owner.subviews.forEach { $0.removeFromSuperview() }
+                    owner.configure()
+                }
+            }).disposed(by: disposeBag)
+        
+        collectionView.rx.itemSelected
+            .subscribe(onNext: { indexPath in
+                let cell = self.collectionView.cellForItem(at: indexPath) as? ItemRow
+                cell?.markCheck()
+            }).disposed(by: disposeBag)
+        
+        resetButton.rx.tap
+            .subscribe(onNext: { _ in
+                let cells = self.collectionView.visibleCells as? [ItemRow]
+                cells?.forEach { $0.removeCheckMark() }
             }).disposed(by: disposeBag)
     }
 }
--- Animal-Crossing-Wiki/Animal-Crossing-Wiki/Dashboard/Views/shared/ItemRow.swift
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/Dashboard/Views/shared/ItemRow.swift
@@ -10,10 +10,11 @@ import UIKit
 class ItemRow: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
         imageView.image = nil
+        removeCheckMark()
     }
 
     func setImage(icon: String) {
@@ -29,5 +30,29 @@ class ItemRow: UICollectionViewCell {
     
     func toggle() {
         self.imageView.alpha = imageView.alpha == 1 ? 0.5 : 1
+    }
+    
+    func removeCheckMark() {
+        let checkImage = imageView.subviews.last as? UIImageView
+        checkImage?.removeFromSuperview()
+    }
+    
+    func markCheck() {
+        guard imageView.subviews.count <= 1 else {
+            removeCheckMark()
+            return
+        }
+        let checkImage = UIImageView(image: UIImage(systemName: "checkmark.circle.fill"))
+        checkImage.tintColor = .acHeaderBackground
+        checkImage.backgroundColor = .white
+        checkImage.layer.cornerRadius = 10
+        imageView.addSubviews(checkImage)
+        
+        NSLayoutConstraint.activate([
+            checkImage.topAnchor.constraint(equalTo: imageView.topAnchor),
+            checkImage.trailingAnchor.constraint(equalTo: imageView.trailingAnchor),
+            checkImage.widthAnchor.constraint(equalToConstant: 20),
+            checkImage.heightAnchor.constraint(equalToConstant: 20)
+        ])
     }
 }
