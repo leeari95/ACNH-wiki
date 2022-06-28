016aa22918294ef402444f5365a08437b6cced97
Ari <lee_ari95@icloud.com>
2022-06-28 15:24:48 +0900

    🔧 [chore] AboutVC 생성 및 RxDataSources 라이브러리 추가 설치

---
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/Dashboard/AboutViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AboutViewController.swift
+//  Animal-Crossing-Wiki
+//
+//  Created by Ari on 2022/06/28.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
