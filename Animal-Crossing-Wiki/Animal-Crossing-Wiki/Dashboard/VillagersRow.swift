01238aa9c36f5e3b5d1a644b90936c519d8c6928
Ari <lee_ari95@icloud.com>
2022-07-01 17:04:21 +0900

    ✨ [feat] VillagersRow에 대한 ViewModel 생성 및 뷰 바인딩 #8
    - 하트나 집 버튼을 누를 때 마다 코어데이터도 갱신하도록 구현
    - 가져온 코어데이터에 따라 UI에 변화를 주도록 구현 (버튼 이미지)
    - 셀이 재사용될 때 마다 disposeBag을 재할당 시켜주어 기존 셀의 바인딩을 끊도록 구현

--- Animal-Crossing-Wiki/Animal-Crossing-Wiki/Dashboard/VillagersRow.swift
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/Dashboard/VillagersRow.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class VillagersRow: UICollectionViewCell {
+    
+    private var viewModel: VillagersRowViewModel!
+    private var disposeBag = DisposeBag()
     
     @IBOutlet weak var iconImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
@@ -21,11 +26,46 @@ class VillagersRow: UICollectionViewCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        likeButton.setImage(nil, for: .normal)
+        houseButton.setImage(nil, for: .normal)
+        viewModel = nil
+        disposeBag = DisposeBag()
     }
     
     func setUp(_ villager: Villager) {
         iconImage.setImage(with: villager.iconImage)
         nameLabel.text = villager.translations.localizedName()
+        viewModel = VillagersRowViewModel(villager: villager)
+        bind()
+    }
+    
+    private func bind() {
+        let input = VillagersRowViewModel.Input(
+            didTapHeart: likeButton.rx.tap.asObservable(),
+            didTapHouse: houseButton.rx.tap.asObservable()
+        )
+        
+        let output = viewModel.transform(input: input, disposeBag: disposeBag)
+        
+        output.isLiked
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { isLiked in
+                if isLiked {
+                    self.likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+                } else {
+                    self.likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+                }
+            }).disposed(by: disposeBag)
+        
+        output.isResident
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { isResident in
+                if isResident {
+                    self.houseButton.setImage(UIImage(systemName: "house.fill"), for: .normal)
+                } else {
+                    self.houseButton.setImage(UIImage(systemName: "house"), for: .normal)
+                }
+            }).disposed(by: disposeBag)
     }
 
 }
