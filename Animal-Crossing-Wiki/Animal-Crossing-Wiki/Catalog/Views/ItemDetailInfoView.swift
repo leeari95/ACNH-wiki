c0c88d96c90c478814eb6dab8580d672cc9c3917
Ari <lee_ari95@icloud.com>
2022-07-16 17:55:35 +0900

    ♻️ [refactor] 아이템의 상세이미지와 가격정보를 보여주는 뷰 리팩토링 #18
    - 기존에 if문으로 분기처리되어있던 부분을 switch문으로 바꾸어 카테고리별로 분기처리하도록 수정
    - 상단 큰 이미지를 속성값으로 분리 (itemLargeImage)
    - 외부에서 상단 큰 이미지를 변경할 수 있는 기능 추가

--- Animal-Crossing-Wiki/Animal-Crossing-Wiki/Catalog/Views/ItemDetailInfoView.swift
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/Catalog/Views/ItemDetailInfoView.swift
@@ -43,6 +43,12 @@ class ItemDetailInfoView: UIView {
         return stackView
     }()
     
+    private lazy var itemLargeImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     convenience init(item: Item) {
         self.init(frame: .zero)
         configure(in: item.category, item: item)
@@ -66,66 +72,85 @@ class ItemDetailInfoView: UIView {
         stackView.axis = .horizontal
         stackView.alignment = .fill
         stackView.distribution = .fill
-        stackView.spacing = 4
+        stackView.spacing = 16
         return stackView
     }
     
     private func setUpImageView(_ item: Item) {
-        if Category.critters.contains(item.category) {
-            let critterpediaImageView = UIImageView(path: item.critterpediaImage ?? "")
+        switch item.category {
+        case .bugs, .fishes, .seaCreatures:
+            itemLargeImage.setImage(with: item.critterpediaImage ?? "")
             let furnitureImageView = UIImageView(path: item.furnitureImage ?? "")
             let iconImageView = UIImageView(path: item.iconImage ?? "")
             
             NSLayoutConstraint.activate([
-                critterpediaImageView.widthAnchor.constraint(equalToConstant: ImageSize.large),
-                critterpediaImageView.heightAnchor.constraint(equalTo: critterpediaImageView.widthAnchor),
+                itemLargeImage.widthAnchor.constraint(equalToConstant: ImageSize.large),
+                itemLargeImage.heightAnchor.constraint(equalTo: itemLargeImage.widthAnchor),
                 furnitureImageView.widthAnchor.constraint(equalToConstant: ImageSize.medium),
                 furnitureImageView.heightAnchor.constraint(equalTo: furnitureImageView.widthAnchor),
                 iconImageView.widthAnchor.constraint(equalToConstant: ImageSize.medium),
                 iconImageView.heightAnchor.constraint(equalTo: iconImageView.widthAnchor)
             ])
             subImageStackView.addArrangedSubviews(furnitureImageView, iconImageView)
-            backgroundStackView.addArrangedSubviews(critterpediaImageView, subImageStackView)
-        } else if let highResTexture = item.highResTexture {
-            let highResTextureImageView = UIImageView(path: highResTexture)
-            backgroundStackView.addArrangedSubviews(highResTextureImageView)
-            NSLayoutConstraint.activate([
-                highResTextureImageView.widthAnchor.constraint(equalTo: backgroundStackView.widthAnchor),
-                highResTextureImageView.heightAnchor.constraint(equalTo: highResTextureImageView.widthAnchor)
-            ])
-        } else {
-            let imageView = UIImageView(path: item.image ?? "")
-            imageView.widthAnchor.constraint(equalToConstant: ImageSize.large).isActive = true
-            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
-            backgroundStackView.addArrangedSubviews(imageView)
+            backgroundStackView.addArrangedSubviews(itemLargeImage, subImageStackView)
+        case .art:
+            if let highResTexture = item.highResTexture {
+                itemLargeImage.setImage(with: highResTexture)
+                backgroundStackView.addArrangedSubviews(itemLargeImage)
+                NSLayoutConstraint.activate([
+                    itemLargeImage.widthAnchor.constraint(equalTo: backgroundStackView.widthAnchor),
+                    itemLargeImage.heightAnchor.constraint(equalTo: itemLargeImage.widthAnchor)
+                ])
+            } else {
+                itemLargeImage.setImage(with: item.image ?? "")
+                itemLargeImage.widthAnchor.constraint(equalToConstant: ImageSize.large).isActive = true
+                itemLargeImage.heightAnchor.constraint(equalTo: itemLargeImage.widthAnchor).isActive = true
+                backgroundStackView.addArrangedSubviews(itemLargeImage)
+            }
+        default:
+            itemLargeImage.setImage(with: item.image ?? "")
+            itemLargeImage.widthAnchor.constraint(equalToConstant: ImageSize.large).isActive = true
+            itemLargeImage.heightAnchor.constraint(equalTo: itemLargeImage.widthAnchor).isActive = true
+            backgroundStackView.addArrangedSubviews(itemLargeImage)
         }
     }
     
     private func setUpItemBells(_ item: Item) {
-        if [Category.seaCreatures, Category.fishes].contains(item.category) {
-            let sell = ItemBellsView(mode: .sell, price: item.sell)
-            let cjSell = ItemBellsView(mode: .cj, price: Int(Double(item.sell) * 1.5))
-            let infoStackView = infoStackView()
-            infoStackView.spacing = 16
-            infoStackView.addArrangedSubviews(sell, cjSell)
-            backgroundStackView.addArrangedSubviews(infoStackView)
-        } else if item.category == .bugs {
+        let infoStackView = infoStackView()
+        switch item.category {
+        case .bugs:
             let sell = ItemBellsView(mode: .sell, price: item.sell)
             let flickSell = ItemBellsView(mode: .flick, price: Int(Double(item.sell) * 1.5))
-            let infoStackView = infoStackView()
-            infoStackView.spacing = 16
             infoStackView.addArrangedSubviews(sell, flickSell)
-            backgroundStackView.addArrangedSubviews(infoStackView)
-        } else if let buy = item.buy, buy > 0 {
+        case .fishes, .seaCreatures:
+            let sell = ItemBellsView(mode: .sell, price: item.sell)
+            let cjSell = ItemBellsView(mode: .cj, price: Int(Double(item.sell) * 1.5))
+            infoStackView.addArrangedSubviews(sell, cjSell)
+        case .housewares, .miscellaneous, .wallMounted, .wallpaper, .floors, .rugs, .ceilingDecor:
+            if item.canExchangePoki, let price = item.exchangePrice {
+                let priceLabel = ItemBellsView(mode: .poki, price: price)
+                infoStackView.addArrangedSubviews(priceLabel)
+            } else if item.canExchangeNookMiles, let price = item.exchangePrice {
+                let price = ItemBellsView(mode: .miles, price: price)
+                infoStackView.addArrangedSubviews(price)
+            } else if item.canExchangeNookPoints, let price = item.exchangePrice {
+                let price = ItemBellsView(mode: .point, price: price)
+                infoStackView.addArrangedSubviews(price)
+            }
+        default: break
+        }
+        if let buy = item.buy, buy > 0, item.isCritters == false {
             let buy = ItemBellsView(mode: .buy, price: buy)
             let sell = ItemBellsView(mode: .sell, price: item.sell)
-            let infoStackView = infoStackView()
-            infoStackView.spacing = 16
             infoStackView.addArrangedSubviews(buy, sell)
-            backgroundStackView.addArrangedSubviews(infoStackView)
-        } else {
+        } else if item.isCritters == false {
             let sell = ItemBellsView(mode: .sell, price: item.sell)
-            backgroundStackView.addArrangedSubviews(sell)
+            infoStackView.addArrangedSubviews(sell)
         }
+        backgroundStackView.addArrangedSubviews(infoStackView)
+    }
+    
+    func changeImage(_ image: UIImage) {
+        itemLargeImage.image = image
     }
 }
