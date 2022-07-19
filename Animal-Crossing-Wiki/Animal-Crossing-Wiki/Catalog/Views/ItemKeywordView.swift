f4774b004c3d686f99f63358d96bdbc7e47cdd37
Ari <lee_ari95@icloud.com>
2022-07-19 17:18:32 +0900

    ✨ [feat]  키워드 섹션 뷰, 키워드 셀, 로딩뷰를 구현 #18
    - 로딩화면에 사용할 로딩뷰 구현
    - 키워드에 따라 Row List를 보여주는 ItemKeywordView를 구현

---
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/Catalog/Views/ItemKeywordView.swift
@@ -0,0 +1,60 @@
+//
+//  ItemKeywordView.swift
+//  Animal-Crossing-Wiki
+//
+//  Created by Ari on 2022/07/14.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ItemKeywordView: UIView {
+    
+    private let disposeBag = DisposeBag()
+    
+    private lazy var collectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 20)
+        flowLayout.minimumInteritemSpacing = 20
+        flowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        flowLayout.minimumLineSpacing = 20
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.backgroundColor = .clear
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.registerNib(KeywordCell.self)
+        return collectionView
+    }()
+    
+    var didTapKeyword: Observable<String> {
+        return collectionView.rx.modelSelected(String.self).asObservable()
+    }
+    
+    convenience init(item: Item) {
+        self.init(frame: .zero)
+        configure(in: item)
+    }
+    
+    private func configure(in item: Item) {
+        addSubviews(collectionView)
+        let heightConstraint = self.collectionView.heightAnchor.constraint(equalToConstant: 50)
+        heightConstraint.priority = .defaultHigh
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: topAnchor, constant: 4),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -4),
+            collectionView.widthAnchor.constraint(equalTo: widthAnchor),
+            heightConstraint
+        ])
+        
+        let keyword = Set(item.keyword)
+
+        Observable.just(keyword)
+            .bind(to: collectionView.rx.items(cellIdentifier: KeywordCell.className, cellType: KeywordCell.self)) { _, text, cell in
+                cell.setUp(title: text)
+            }.disposed(by: disposeBag)
+    }
+}
