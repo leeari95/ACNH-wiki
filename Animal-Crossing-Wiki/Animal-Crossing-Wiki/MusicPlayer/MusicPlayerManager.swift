f3ad2b1f56aa5f5a68147991aa6a1f9de19ed2af
Ari <lee_ari95@icloud.com>
2022-07-22 17:19:54 +0900

    ✨ [feat] 음악 플레이어 매니저를 생성 #19

---
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/MusicPlayer/MusicPlayerManager.swift
@@ -0,0 +1,205 @@
+//
+//  MusicPlayerManager.swift
+//  Animal-Crossing-Wiki
+//
+//  Created by Ari on 2022/07/22.
+//
+
+import Foundation
+import OSLog
+import RxSwift
+import RxRelay
+import AVFoundation
+
+final class MusicPlayerManager {
+    
+    enum PlayerMode {
+        case shuffle
+        case fullRepeat
+        case oneSongRepeat
+    }
+    
+    static let shared = MusicPlayerManager()
+    
+    private let disposeBag = DisposeBag()
+    private var player: AVPlayer?
+    private var timer: Timer?
+    
+    private let isPlaying = BehaviorRelay<Bool?>(value: nil)
+    private let currentSong = BehaviorRelay<Item?>(value: nil)
+    private var songs = BehaviorRelay<[String: Item]>(value: [:])
+    private var songsItem = BehaviorRelay<[Item]>(value: [])
+    private let playerMode = BehaviorRelay<PlayerMode>(value: .shuffle)
+    
+    private let playerProgress = BehaviorRelay<Float>(value: 0)
+    private let durationTime = BehaviorRelay<String>(value: "0:00")
+    private let elapsedTime = BehaviorRelay<String>(value: "0:00")
+    
+    private init() {
+        Items.shared.categoryList
+            .compactMap { $0[.songs] }
+            .withUnretained(self)
+            .subscribe(onNext: { owner, songs in
+                var newSongs = [String: Item]()
+                songs.forEach { item in
+                    newSongs[item.name] = item
+                }
+                owner.songs.accept(newSongs)
+                owner.songsItem.accept(songs.shuffled())
+                owner.currentSong.accept(owner.songsItem.value.first)
+            }).disposed(by: disposeBag)
+        
+        currentSong
+            .compactMap { $0?.musicURL }
+            .compactMap { URL(string: $0) }
+            .withUnretained(self)
+            .subscribe(onNext: { owner, musicURL in
+                owner.player = AVPlayer(url: musicURL)
+            }).disposed(by: disposeBag)
+        
+        isPlaying
+            .compactMap { $0 }
+            .withUnretained(self)
+            .subscribe(onNext: { owner, isPlaying in
+                if isPlaying {
+                    owner.player?.play()
+                } else {
+                    owner.player?.pause()
+                }
+                owner.setUpPlayerTimer()
+            }).disposed(by: disposeBag)
+        
+        NotificationCenter.default.addObserver(
+            forName: .AVPlayerItemDidPlayToEndTime,
+            object: player?.currentItem,
+            queue: .main
+        ) { [weak self] _ in
+            self.flatMap { owner in
+                owner.playerProgress.accept(0)
+                owner.elapsedTime.accept("0:00")
+                owner.isPlaying.accept(false)
+                switch owner.playerMode.value {
+                case .fullRepeat:
+                    owner.next()
+                case .shuffle:
+                    owner.currentSong.accept(owner.songsItem.value.randomElement())
+                    owner.isPlaying.accept(true)
+                case .oneSongRepeat:
+                    owner.currentSong.accept(owner.currentSong.value)
+                    owner.isPlaying.accept(true)
+                }
+            }
+        }
+    }
+    
+    private func changeSong(at newIndex: Int) {
+        playerProgress.accept(0)
+        elapsedTime.accept("0:00")
+        guard let current = currentSong.value,
+              var index = songsItem.value.firstIndex(of: current) else {
+            os_log(.error, log: .default, "⛔️ 음악을 가져오는데 실패했습니다.")
+            return
+        }
+        index += newIndex
+        if let newSong = songsItem.value[safe: index] {
+            currentSong.accept(newSong)
+        } else if index < 0 {
+            let newSong = songsItem.value.last
+            currentSong.accept(newSong)
+        } else {
+            let newSong = songsItem.value.first
+            currentSong.accept(newSong)
+        }
+        isPlaying.accept(true)
+    }
+    
+    private func updatePlayingInfo() {
+        if let duration = player?.currentItem?.duration.seconds,
+            let playTime = player?.currentItem?.currentTime().seconds,
+            !duration.isNaN, !playTime.isNaN {
+            playerProgress.accept(Float(playTime) / Float(duration))
+            
+            let durationSecs = Int(duration)
+            let durationSeconds = Int(durationSecs % 3600 ) % 60
+            let durationMinutes = Int(durationSecs % 3600) / 60
+            let durationTime = "\(durationMinutes):\(String(format: "%02d", durationSeconds))"
+            self.durationTime.accept(durationTime)
+            
+            let playTimeSecs = Int(playTime)
+            let playTimeSeconds = Int(playTimeSecs % 3600) % 60
+            let playTimeMinutes = Int(playTimeSecs % 3600) / 60
+            let elapsedTime = "\(playTimeMinutes):\(String(format: "%02d", playTimeSeconds))"
+            self.elapsedTime.accept(elapsedTime)
+        }
+    }
+    
+    private func setUpPlayerTimer() {
+        if isPlaying.value == true {
+            if timer != nil {
+                timer?.invalidate()
+                timer = nil
+            }
+            timer = Timer.scheduledTimer(
+                withTimeInterval: 0.5,
+                repeats: true,
+                block: { [weak self] _ in
+                    self?.updatePlayingInfo()
+                }
+            )
+        } else {
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+
+}
+
+extension MusicPlayerManager {
+    
+    var isNowPlaying: Observable<Bool?> {
+        return isPlaying.asObservable()
+    }
+    
+    var currentMusic: Observable<Item?> {
+        return currentSong.asObservable()
+    }
+    
+    var currentTime: Observable<String> {
+        return elapsedTime.asObservable()
+    }
+    
+    var fullTime: Observable<String> {
+        return durationTime.asObservable()
+    }
+    
+    var songProgress: Observable<Float> {
+        return playerProgress.asObservable()
+    }
+    
+    func togglePlaying() {
+        isPlaying.accept(isPlaying.value == true ? false : true)
+    }
+    
+    func togglePlayerMode() {
+        if playerMode.value == .shuffle {
+            playerMode.accept(.fullRepeat)
+        } else if playerMode.value == .oneSongRepeat {
+            playerMode.accept(.shuffle)
+        } else {
+            playerMode.accept(.oneSongRepeat)
+        }
+    }
+    
+    func next() {
+        changeSong(at: 1)
+    }
+    
+    func prev() {
+        changeSong(at: -1)
+    }
+    
+    func choice(_ item: Item) {
+        currentSong.accept(item)
+        isPlaying.accept(true)
+    }
+}
