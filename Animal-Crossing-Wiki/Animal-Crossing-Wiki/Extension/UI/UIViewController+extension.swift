7f7d5bc9aca5fd7fff98619600ad25c443da8f42
Ari <lee_ari95@icloud.com>
2022-07-27 13:55:59 +0900

    🐛 [fix] 아이패드에서 Action Sheet를 표시할 때 앱이 다운되는 현상을 개선

--- Animal-Crossing-Wiki/Animal-Crossing-Wiki/Extension/UI/UIViewController+extension.swift
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/Extension/UI/UIViewController+extension.swift
@@ -10,7 +10,11 @@ import RxSwift
 
 extension UIViewController {
     
-    func showSelectedItemAlert(_ items: [String], currentItem: String?) -> Observable<String> {
+    func showSelectedItemAlert(
+        _ items: [String],
+        currentItem: String?,
+        barButtonItem: UIBarButtonItem? = nil
+    ) -> Observable<String> {
         return Observable.create { observer in
             let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
             items.forEach { item in
@@ -27,6 +31,20 @@ extension UIViewController {
                     alert.dismiss(animated: true)
                 }
             )
+            if let popoverController = alert.popoverPresentationController {
+                popoverController.sourceView = self.view
+                if let barButtonItem = barButtonItem {
+                    popoverController.barButtonItem = barButtonItem
+                } else {
+                    popoverController.sourceRect = CGRect(
+                        x: self.view.bounds.midX ,
+                        y: self.view.bounds.midY,
+                        width: .zero,
+                        height: .zero
+                    )
+                    popoverController.permittedArrowDirections = []
+                }
+            }
             self.present(alert, animated: true, completion: nil)
             return Disposables.create { alert.dismiss(animated: true, completion: nil) }
         }
