b13df6d7b20dd2079cebe6d5b7bc0ba96e715352
Ari <lee_ari95@icloud.com>
2022-11-21 17:11:05 +0900

    ✨ [feat] 우산에 대한 Request, DTO 타입 구현

---
+++ Animal-Crossing-Wiki/Animal-Crossing-Wiki/Sources/Networking/Response/UmbrellasResponseDTO.swift
@@ -0,0 +1,60 @@
+//
+//  UmbrellasResponseDTO.swift
+//  Animal-Crossing-Wiki
+//
+//  Created by Ari on 2022/11/21.
+//
+
+import Foundation
+
+struct UmbrellasResponseDTO: Codable {
+    let name: String
+    let storageImage: String
+    let diy: Bool
+    let buy, sell, hhaBasePoints: Int
+    let size: Size
+    let exchangePrice: Int?
+    let exchangeCurrency: ExchangeCurrency?
+    let source: [String]
+    let sourceNotes: [String]?
+    let seasonEvent: String?
+    let seasonEventExclusive: Bool?
+    let villagerEquippable: Bool
+    let catalog: Catalog
+    let versionAdded: String
+    let unlocked: Bool
+    let filename: String
+    let internalId: Int
+    let uniqueEntryId: String
+    let translations: Translations
+    let colors: [Color]
+    let recipe: RecipeResponseDTO?
+}
+
+extension UmbrellasResponseDTO {
+    func toDomain() -> Item {
+        return Item(
+            name: name,
+            category: .bags,
+            image: storageImage,
+            diy: diy,
+            buy: buy,
+            sell: sell,
+            size: size,
+            exchangePrice: exchangePrice,
+            exchangeCurrency: exchangeCurrency,
+            sources: source,
+            sourceNotes: sourceNotes,
+            seasonEvent: seasonEvent,
+            hhaBasePoints: hhaBasePoints,
+            catalog: catalog,
+            internalId: internalId,
+            translations: translations,
+            colors: colors,
+            themes: nil,
+            styles: [],
+            recipe: recipe,
+            variations: nil
+        )
+    }
+}
